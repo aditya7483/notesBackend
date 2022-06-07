35fa3e54ddb53aa8182c687bf9bf8b9d3c4eb7ca
Aditya Narayan Jha <adityanarayanjha31@gmail.com>
2022-06-07 20:13:46 +0530

    changed port

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 // const fetch = require('node-fetch');
 const express = require('express');
 const app= express();
-const port= 3000;
+const port= 3001;
 const connectToMongo = require('./modules/mongoose');
 const Note = require('./schemas/note');
 
@@ -9,12 +9,12 @@ connectToMongo();
 
 app.use(express.json());
 
-app.get('/notes',async (req,res)=>{
+app.get('/api/notes',async (req,res)=>{
     let data= await Note.find();
     res.json(data);
 })
 
-app.post("/createNote", (req, res) => {
+app.post("/api/createNote", (req, res) => {
     let newNote = Note(req.body);
     newNote.save((err,data)=>{
         if(err) res.send(err);
@@ -23,7 +23,7 @@ app.post("/createNote", (req, res) => {
     });
 });
 
-app.put('/update/:id',async (req,res)=>{
+app.put('/api/update/:id',async (req,res)=>{
     let note = await Note.findById(req.params.id);
     if(!note){
         res.status(404).send("not found");
