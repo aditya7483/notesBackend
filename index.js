dda669ad649215de9e81c12e9c99b348a7a66a26
Aditya Narayan Jha <adityanarayanjha31@gmail.com>
2022-06-10 15:43:11 +0530

    CORS related changes

--- index.js
+++ index.js
@@ -9,6 +9,16 @@ connectToMongo();
 
 app.use(express.json());
 
+var allowCrossDomain = function(req, res, next) {
+    res.header('Access-Control-Allow-Origin', "*");
+    res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
+    res.header('Access-Control-Allow-Headers', 'Content-Type');
+    next();
+}
+
+    app.use(allowCrossDomain);
+    
+
 app.get('/api/notes',async (req,res)=>{
     let data= await Note.find();
     res.json(data);
