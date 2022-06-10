7c9c07da52476161fc49dbec351af5a76b17f8f4
Aditya Narayan Jha <adityanarayanjha31@gmail.com>
2022-06-10 06:53:13 +0530

    herok changes

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 // const fetch = require('node-fetch');
 const express = require('express');
 const app= express();
-const port= 3001;
+const port= process.env.PORT || 3001;
 const connectToMongo = require('./modules/mongoose');
 const Note = require('./schemas/note');
 
